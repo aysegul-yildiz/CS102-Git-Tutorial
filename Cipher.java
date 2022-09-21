af2349c52d379bf8b0bc2e8dde4f27ef5a67d76a
aysegul.yldz <aysegul.yildiz@ug.bilkent.edu.tr>
2022-09-21 11:12:57 +0300

    change 1

--- Cipher.java
+++ Cipher.java
@@ -15,7 +15,8 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-
+            // append the encrypted version of the char to the output string
+            outputString += replaceChar(inputString.charAt(i), true);
         }
 
         return outputString;
